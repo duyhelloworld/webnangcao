a271be8509f154d90e8effe20c01c149837abf00
duyhelloworld <codedaovoiduy@gmail.com>
2023-10-22 17:46:29 +0700

    1 file để tạo cả db, user-pass, data

---
+++ Properties/data.sql
@@ -0,0 +1,10 @@
+-- Active: 1695612038229@@127.0.0.1@1433@master
+CREATE DATABASE webnangcao;
+USE [webnangcao];
+SELECT * FROM sys.databases WHERE name = 'webnangcao';
+CREATE LOGIN [webnangcao] WITH PASSWORD = '12345678', DEFAULT_DATABASE = [webnangcao], CHECK_POLICY = OFF;
+CREATE USER [webnangcao] FOR LOGIN [webnangcao];
+EXEC sp_addrolemember 'db_owner', 'webnangcao';
+GRANT CREATE, SELECT, INSERT, UPDATE, DELETE ON SCHEMA::dbo TO [webnangcao];
+
+-- INSERT INTO () VALUES()
