a9f33ef72436b6b18127b4c786fe035d693a7dd5
duyhelloworld <codedaovoiduy@gmail.com>
2023-10-25 22:39:12 +0700

    Thêm giới hạn độ dài cột

--- Entities/Track.cs
+++ Entities/Track.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using webnangcao.Entities.Enumerables;
 using webnangcao.Entities.Joins;
 
 namespace webnangcao.Entities;
@@ -8,23 +9,32 @@ public class Track
     [Key]
     public int Id { get; set; }
 
+    [StringLength((int)EMaxValue.TrackNameLength)]
     public string Name { get; set; } = null!;
 
-    [Required]
+    [StringLength((int)EMaxValue.DirectoryLength)]
     public string Directory { get; set; } = null!;
+
 
     [DataType(DataType.DateTime)]
     public DateTime UploadAt { get; set; } = DateTime.Now;
 
+    [MaxLength]
     public string? Description { get; set; }
 
-    public string? ArtWork { get; set; }
 
-    // Cache lại để query nhanh hơn
-    public int SoLuotNghe { get; set; }
-    public int SoLuotThich { get; set; }
-    public int SoLuotBinhLuan { get; set; }
+    [StringLength((int)EMaxValue.DirectoryLength)]
+    public string? ArtWorkDirectory { get; set; }
 
-    public ICollection<Track_Category> Categories { get; set; } = new HashSet<Track_Category>();
-    public ICollection<Track_Playlist> Playlists { get; set; } = new HashSet<Track_Playlist>();
+
+    public ICollection<Track_Category> Categories { get; set; } 
+        = new HashSet<Track_Category>();
+    
+    public ICollection<Comment> Comments { get; set; } 
+        = new HashSet<Comment>();
+
+    public ICollection<Track_Playlist> Playlists { get; set; } 
+        = new HashSet<Track_Playlist>();
+    public ICollection<UserTrackAction> UserActions { get; set; } 
+        = new HashSet<UserTrackAction>();
 }
