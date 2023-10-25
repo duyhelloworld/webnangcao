9342da48aad7013e5f1f6e612258f435e7155609
duyhelloworld <codedaovoiduy@gmail.com>
2023-10-25 22:36:27 +0700

    Cút, đưa vào [Tools](/Tools) rồi

--- Tools/ERoleTool.cs
+++ Tools/ERoleTool.cs
@@ -1,6 +1,8 @@
-namespace webnangcao.Entities.Enumerables;
+using webnangcao.Entities.Enumerables;
 
-public class ERoleConverter
+namespace webnangcao.Tools;
+
+public class ERoleTool
 {
     public static ERole ToERole(string roleString)
     {
@@ -16,5 +18,15 @@ public class ERoleConverter
     public static string ToString(ERole erole)
     {
         return erole.ToString();
+    }
+
+    public static ERole GetHighestRole(string[] roles)
+    {
+        if (roles.Contains("SuperAdmin"))
+            return ERole.SuperAdmin;
+        else if (roles.Contains("Admin"))
+            return ERole.Admin;
+        else
+            return ERole.User;
     }
 }
