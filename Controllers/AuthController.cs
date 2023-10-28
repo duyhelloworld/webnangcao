c25216e8e1daa8ec421dedd367b34438e81642b5
duyhelloworld <codedaovoiduy@gmail.com>
2023-10-28 21:47:10 +0700

    !!! Đang lú, login bình thường mà đéo chạy api đc

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using webnangcao.Entities;
 using webnangcao.Entities.Enumerables;
 using webnangcao.Models.Securities;
 using webnangcao.Services;
 
 namespace webnangcao.Controllers;
 
-[Route("[controller]")]
 [ApiController]
+[Route("[controller]")]
 public class AuthController : ControllerBase
 {
     private readonly IAuthService _service;
@@ -19,20 +20,21 @@ public class AuthController : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-        return BadRequest("Truy cập postman.com để dùng POST");
+        return BadRequest("Truy cập <a href=\"postman.com\">Postman</a> để dùng POST");
     }
 
-    [HttpPost("signup")]
-    public async Task<IActionResult> SignUp([FromBody] SignupModel model)
+    [AllowAnonymous]
+    [HttpPost("signup/user")]
+    public async Task<IActionResult> SignUpUser([FromBody] SignupModel model)
     {
-        return Ok(await _service.SignUpAsync(model, "User"));
+        return Ok(await _service.SignUpAsync(model, ERole.USER));
     }
 
-    [Authorize(Roles = nameof(ERole.Admin) + "," + nameof(ERole.SuperAdmin))]
-    [HttpPost("signup/{role:minlength(1)}")]
-    public async Task<IActionResult> SignUp([FromBody] SignupModel model, [FromRoute] string role)
+    [Authorize(Roles = nameof(ERole.SUPERADMIN))]
+    [HttpPost("signup/admin")]
+    public async Task<IActionResult> SignUpAmin([FromBody] SignupModel model)
     {
-        return Ok(await _service.SignUpAsync(model, role));
+        return Ok(await _service.SignUpAsync(model, ERole.ADMIN));
     }
 
     [HttpPost("login")]
@@ -40,4 +42,18 @@ public class AuthController : ControllerBase
     {
         return Ok(await _service.SignInAsync(model));
     }
+
+    // [HttpGet("hello")]
+    // [Authorize(Roles = "Admin")]
+    // public IActionResult Hello()
+    // {
+    //     return Ok("Hello");
+    // }
+
+    // [HttpGet("hi")] 
+    // [Authorize(Roles = nameof(ERole.SUPERADMIN))]
+    // public IActionResult Hi()
+    // {
+    //     return Ok("Hi");
+    // }
 }
