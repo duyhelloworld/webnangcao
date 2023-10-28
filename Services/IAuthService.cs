3c58e689b2655ea6e59af5a697cadcceebd94d7c
duyhelloworld <codedaovoiduy@gmail.com>
2023-10-28 21:56:21 +0700

    Đổi string của role thành enum hết

--- Services/IAuthService.cs
+++ Services/IAuthService.cs
@@ -8,5 +8,5 @@ public interface IAuthService
     Task<ResponseModel> SignInAsync(LoginModel model);
 
     // Mặc định nếu signup không có role chỉ định thì luôn là User
-    Task<ResponseModel> SignUpAsync(SignupModel model, string role);
+    Task<ResponseModel> SignUpAsync(SignupModel model, ERole erole);
 }
