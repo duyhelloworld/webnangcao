be77cd15c271f2393340d9fee48cb9fd50187864
duyhelloworld <codedaovoiduy@gmail.com>
2023-10-28 21:48:45 +0700

    Ràng buộc rõ hơn độ dài string

--- Entities/Category.cs
+++ Entities/Category.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using webnangcao.Entities.Enumerables;
 using webnangcao.Entities.Joins;
 
 namespace webnangcao.Entities;
@@ -8,9 +9,10 @@ public class Category
     [Key]
     public int Id { get; set; }
 
-    [StringLength(50)]
+    [StringLength((int) EMaxValue.CategoryNameLength)]
     public string Name { get; set; } = null!;
     
+    [MaxLength]
     public string? Description { get; set; }
 
     public ICollection<Track_Category> Tracks { get; set; } = new HashSet<Track_Category>();
