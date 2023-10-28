1ed93dfe6aba32d6cfc2bf796128c87a5a0806e1
duyhelloworld <codedaovoiduy@gmail.com>
2023-10-28 17:17:50 +0700

    Cố định chiều dài tên role

--- Entities/Role.cs
+++ Entities/Role.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
+using webnangcao.Entities.Enumerables;
 
 namespace webnangcao.Entities;
 
 public class Role : IdentityRole
 {
-    
+    [StringLength((int)EMaxValue.RoleLength)]
+    public override string Id { get; set ; } = Guid.NewGuid().ToString();
 }
