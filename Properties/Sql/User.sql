cc3b4970ee244d3d115fd1ca9902b5eb52a1afd2
duyhelloworld <codedaovoiduy@gmail.com>
2023-11-06 09:47:00 +0700

    Chuyển tới @Assets/sql

--- Properties/Sql/User.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE webnangcao;
-USE [webnangcao];
-CREATE LOGIN [webnangcao] WITH PASSWORD = '12345678', DEFAULT_DATABASE = [webnangcao], CHECK_POLICY = OFF;
-CREATE USER [webnangcao] FOR LOGIN [webnangcao];
-EXEC sp_addrolemember 'db_owner', 'webnangcao';
-GRANT CREATE TABLE, CREATE PROCEDURE, CREATE FUNCTION TO [webnangcao];
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA::dbo TO [webnangcao];
