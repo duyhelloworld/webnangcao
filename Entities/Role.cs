a36ce3b9faecca949cf3240efe972fa210e4d5be
duyhelloworld <codedaovoiduy@gmail.com>
2023-11-06 09:44:59 +0700

    Not null tên role

--- Entities/Role.cs
+++ Entities/Role.cs
@@ -4,5 +4,6 @@ namespace webnangcao.Entities;
 
 public class Role : IdentityRole<long>
 {
-
+    public override string Name { get ; set ; }  = null!;
+    public override string? NormalizedName { get ; set ; }
 }
