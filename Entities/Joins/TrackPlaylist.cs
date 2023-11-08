8fad5dfff95b984323f1bc2271014363b5fdd098
duyhelloworld <codedaovoiduy@gmail.com>
2023-11-08 11:34:30 +0700

    Ràng buộc để bảo đảm xoá track ko ảnh hưởng playlist và ngược lại

--- Entities/Joins/TrackPlaylist.cs
+++ Entities/Joins/TrackPlaylist.cs
@@ -8,8 +8,10 @@ namespace webnangcao.Entities.Joins;
 public class TrackPlaylist
 {
     public int TrackId { get; set; }
+    [DeleteBehavior(DeleteBehavior.NoAction)]
     public Track Track { get; set; } = null!;
 
     public int PlaylistId { get; set; }
+    [DeleteBehavior(DeleteBehavior.NoAction)]
     public Playlist Playlist { get; set; } = null!;
 }
