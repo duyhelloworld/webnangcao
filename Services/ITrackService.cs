1969d586c79cf865546d30079da7a0d3bd488067
duyhelloworld <codedaovoiduy@gmail.com>
2023-11-11 22:12:26 +0700

    1 vài task chung

--- Services/ITrackService.cs
+++ Services/ITrackService.cs
@@ -8,11 +8,16 @@ namespace webnangcao.Services;
 
 public interface ITrackService
 {
-    public Task<IEnumerable<TrackResponseModel>> GetAll();
-    public Task<TrackResponseModel?> GetById(int id);
-    // public Task<IEnumerable<TrackResponseModel>> Search(string input);
+    // Get public
+    public Task<IEnumerable<TrackResponseModel>> GetTopNewest();
+    public Task<IEnumerable<TrackResponseModel>> GetTopTrending();
+
+    // Get private
+    public Task<IEnumerable<TrackResponseModel>> GetAll(long userId);
     
-    public Task AddNew(TrackInsertModel model, long userId);
-    public Task UpdateInfomation(TrackUpdateModel model, int id);
-    public Task Remove(int id);
+    public Task<TrackResponseModel?> GetById(int id);
+    
+    public Task AddNew(TrackInsertModel model, IFormFile fileTrack, IFormFile fileArtwork, long userId);
+    public Task UpdateInfomation(TrackUpdateModel model, IFormFile fileArtwork, int trackId);
+    public Task Remove(int trackId);
 }
