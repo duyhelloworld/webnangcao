ad371f6c37ea549f0b65f70f3d23211f8a21a3ac
-g <-g>
2023-12-12 21:15:37 +0700

    Migration mới

--- Migrations/20231212135534_NewMigra.cs
+++ Migrations/20231212135534_NewMigra.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace webnangcao.Migrations
 {
     /// <inheritdoc />
-    public partial class TestApp : Migration
+    public partial class NewMigra : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
