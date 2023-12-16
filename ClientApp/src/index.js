4b554a396f33957138271f55148edc2c254ad599
wanhoang777 <quan0218566@huce.edu.vn>
2023-12-16 22:33:20 +0700

    Changes to be committed:

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './Login';
-import reportWebVitals from './reportWebVitals';
+import Register from './Components/Register';
+import Login from './Components/Login';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/auth/signup" element={Register} />
+        <Route path="/auth/signin" element={Login} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.render(
   <React.StrictMode>
-    <Login />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 // server.js
 
@@ -17,4 +30,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
